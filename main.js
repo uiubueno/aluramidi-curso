606fe7f39e6e5d2f58bb5b23610ea722b613bb81
William Bueno <contatowillbuenoo@gmail.com>
2024-06-17 20:22:30 -0300

    Alterações no mainJS

---
+++ main.js
@@ -0,0 +1,9 @@
+function TocaSomPom() {
+  document.querySelector("#som_tecla_pom").play();
+}
+
+document.querySelector(".tecla_pom").onclick = TocaSomPom;
+
+function tocaSomClap() {
+  document.querySelector("#som_tecla_clap").play();
+}
