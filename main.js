bed5bd3a83a0bb981fe7687ef407fedd285edd4d
William Bueno <contatowillbuenoo@gmail.com>
2024-06-19 10:17:04 -0300

    mainjs funcionando

--- main.js
+++ main.js
@@ -7,7 +7,19 @@ const listaDeTeclas = document.querySelectorAll(".tecla");
 let contador = 0;
 
 while (contador < listaDeTeclas.length) {
-  listaDeTeclas[contador].onclick = tocaSom;
+  const tecla = listaDeTeclas[contador];
+
+  const instrumento = tecla.classList[1];
+
+  console.log(instrumento);
+
+  const idAudio = `#som_${instrumento}`;
+
+  console.log(idAudio);
+
+  tecla.onclick = function () {
+    tocaSom(idAudio);
+  };
 
   contador = contador + 1;
 
