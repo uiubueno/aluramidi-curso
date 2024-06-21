e2b09d2bda4a6bb61670cee0de8f5e78da4eceee
William Bueno <contatowillbuenoo@gmail.com>
2024-06-21 10:52:40 -0300

    alterações mainjs

--- main.js
+++ main.js
@@ -4,24 +4,12 @@ function tocaSom(idElementoAudio) {
 
 const listaDeTeclas = document.querySelectorAll(".tecla");
 
-let contador = 0;
-
-while (contador < listaDeTeclas.length) {
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
   const tecla = listaDeTeclas[contador];
-
   const instrumento = tecla.classList[1];
-
-  console.log(instrumento);
-
   const idAudio = `#som_${instrumento}`;
-
-  console.log(idAudio);
 
   tecla.onclick = function () {
     tocaSom(idAudio);
   };
-
-  contador = contador + 1;
-
-  console.log(contador);
 }
